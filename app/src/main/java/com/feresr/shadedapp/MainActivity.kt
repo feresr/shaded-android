3444ef309a0ce0142f6bf4c95a94ff56a3a1cb33
Fernando Raviola <fernandoraviola@gmail.com>
2022-09-17 18:33:58 +1000

    Update renderer API

--- app/src/main/java/com/feresr/shadedapp/MainActivity.kt
+++ app/src/main/java/com/feresr/shadedapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.lifecycleScope
 import com.feresr.shaded.Filter
 import com.feresr.shaded.R
 import com.feresr.shaded.R.drawable
-import com.feresr.shaded.Shaded
+import com.feresr.shaded.render
 import com.feresr.shaded.shaders.FilterBlur
 import com.feresr.shaded.shaders.FilterBrightness
 import com.feresr.shaded.shaders.FilterContrast
@@ -36,11 +36,8 @@ import kotlinx.android.synthetic.main.activity_main.rainbow
 import kotlinx.android.synthetic.main.activity_main.removeFilter
 import kotlinx.android.synthetic.main.activity_main.saturationSeekbar
 import kotlinx.android.synthetic.main.activity_main.seekbar
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.launch
-import java.util.*
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -69,7 +66,6 @@ class MainActivity : AppCompatActivity() {
     private var currentBitmap = 0
     private var filterIndex = 0
 
-    private val shaded = Shaded(this)
     private lateinit var original: Bitmap
     private lateinit var canvas: Bitmap
 
@@ -80,39 +76,23 @@ class MainActivity : AppCompatActivity() {
 
     private val hslUniforms = FloatArray(3 * 8) { 0.0f }
 
-    @OptIn(ObsoleteCoroutinesApi::class)
-    val actor = lifecycleScope.actor<ActorMessage>(capacity = Channel.CONFLATED) {
-        val updateFilters: suspend (Int) -> Unit = { progress: Int ->
-            Log.e("progress", progress.toString())
-            hue.updateUniforms(sin(progress.toFloat() / 10f))
-            contrast.updateUniforms(progress.toFloat() / 100f)
-            inverse.updateUniforms(cos(progress.toFloat() / 100f))
-            grain.updateUniforms(sin(progress.toFloat() / 100f))
-            bright.updateUniforms(progress.toFloat() / 100f)
-            exposure.updateUniforms(progress.toFloat()/100f)
-            blur.updateUniforms(
-                sin(progress.toFloat() / 1000f),
-                sin(progress.toFloat() / 1000f),
-            )
-            hslUniforms[colorIndexSelected * 3 + 0] = progress.toFloat() / 100f
-            hsl.updateUniforms(hslUniforms)
-        }
+    private val channel = Channel<ActorMessage>(Channel.CONFLATED)
 
-        for (msg in channel) {
-            when (msg) {
-                is Update -> {
-                    updateFilters(msg.i)
-                    shaded.render(canvas, appliedFilters)
-                }
-                is ActorMessage.Rescale -> {
-                    val bitmap = if (msg.i > 0) canvas else original
-                    updateFilters(msg.progress)
-                    shaded.render(bitmap, appliedFilters)
-                    image.setImageBitmap(bitmap)
-                }
-            }
-            image.invalidate()
-        }
+
+    private fun updateFilters(progress: Int) {
+        Log.e("progress", progress.toString())
+        hue.updateUniforms(sin(progress.toFloat() / 10f))
+        contrast.updateUniforms(progress.toFloat() / 100f)
+        inverse.updateUniforms(cos(progress.toFloat() / 100f))
+        grain.updateUniforms(sin(progress.toFloat() / 100f))
+        bright.updateUniforms(progress.toFloat() / 100f)
+        exposure.updateUniforms(progress.toFloat() / 100f)
+        blur.updateUniforms(
+            sin(progress.toFloat() / 1000f),
+            sin(progress.toFloat() / 1000f),
+        )
+        hslUniforms[colorIndexSelected * 3 + 0] = progress.toFloat() / 100f
+        hsl.updateUniforms(hslUniforms)
     }
 
 
@@ -121,43 +101,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val options = BitmapFactory.Options()
-        options.inScaled = false
-        options.inMutable = true
-
-        val s = Stack<Int>()
-
-        lifecycleScope.launch {
-            original = BitmapFactory.decodeResource(resources, drawable.square, options)
-            shaded.upload(original)
-
-            canvas = Bitmap.createScaledBitmap(
-                original, original.width / 2, original.height / 2, true
-            )
-            image.setImageBitmap(canvas)
-        }
-
         removeFilter.setOnClickListener {
             filterIndex = 0
             appliedFilters.clear()
             lifecycleScope.launch {
-                shaded.render(canvas, appliedFilters)
+                //shaded.render(canvas, appliedFilters)
             }
         }
 
         changeBitmap.setOnClickListener {
             lifecycleScope.launch {
-                original = BitmapFactory.decodeResource(
-                    resources,
-                    bitmaps[currentBitmap % bitmaps.size],
-                    options
-                )
-                shaded.upload(original)
-                canvas = Bitmap.createScaledBitmap(
-                    original, original.width / 2, original.height / 2, true
-                )
-                currentBitmap++
-                shaded.render(canvas, appliedFilters)
+//                original = BitmapFactory.decodeResource(
+//                    resources,
+//                    bitmaps[currentBitmap % bitmaps.size],
+//                    options
+//                )
+//                shaded.upload(original)
+//                canvas = Bitmap.createScaledBitmap(
+//                    original, original.width / 2, original.height / 2, true
+//                )
+//                currentBitmap++
+//                shaded.render(canvas, appliedFilters)
             }
         }
 
@@ -175,10 +139,38 @@ class MainActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 appliedFilters.add(filters[filterIndex % filters.size])
                 filterIndex++
-                shaded.render(canvas, appliedFilters)
+                //shaded.render(canvas, appliedFilters)
             }
         }
 
+        lifecycleScope.launch {
+            render(this@MainActivity) {
+                val options = BitmapFactory.Options()
+                options.inScaled = false
+                options.inMutable = true
+                original = BitmapFactory.decodeResource(resources, drawable.square, options)
+                upload(original)
+                canvas = Bitmap.createScaledBitmap(
+                    original, original.width / 1, original.height / 1, true
+                )
+                image.setImageBitmap(canvas)
+                for (msg in channel) {
+                    when (msg) {
+                        is Update -> {
+                            updateFilters(msg.i)
+                            render(canvas, appliedFilters)
+                        }
+                        is ActorMessage.Rescale -> {
+                            val bitmap = if (msg.i > 0) canvas else original
+                            updateFilters(msg.progress)
+                            render(bitmap, appliedFilters)
+                            image.setImageBitmap(bitmap)
+                        }
+                    }
+                    image.invalidate()
+                }
+            }
+        }
     }
 
     override fun onStop() {
@@ -198,7 +190,7 @@ class MainActivity : AppCompatActivity() {
                     hslUniforms[colorIndexSelected * 3 + 2] = progress.toFloat() / 100f
                     hsl.updateUniforms(hslUniforms)
                     lifecycleScope.launch {
-                        shaded.render(canvas, appliedFilters)
+                        //shaded.render(canvas, appliedFilters)
                         image.setImageBitmap(canvas)
                     }
                 }
@@ -219,8 +211,8 @@ class MainActivity : AppCompatActivity() {
                     hslUniforms[colorIndexSelected * 3 + 1] = progress.toFloat() / 100f
                     hsl.updateUniforms(hslUniforms)
                     lifecycleScope.launch {
-                        shaded.render(canvas, appliedFilters)
-                        image.setImageBitmap(canvas)
+//                        shaded.render(canvas, appliedFilters)
+//                        image.setImageBitmap(canvas)
                     }
                 }
 
@@ -237,27 +229,26 @@ class MainActivity : AppCompatActivity() {
         seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 lifecycleScope.launch {
-                    actor.send(ActorMessage.Update(progress))
+                    channel.send(ActorMessage.Update(progress))
                 }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
                 // Optional: downscale (better performance on large bitmaps)
                 lifecycleScope.launch {
-                    actor.send(ActorMessage.Rescale(1, seekBar.progress))
+                    channel.send(ActorMessage.Rescale(1, seekBar.progress))
                 }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 lifecycleScope.launch {
-                    actor.send(ActorMessage.Rescale(0, seekBar.progress))
+                    channel.send(ActorMessage.Rescale(0, seekBar.progress))
                 }
             }
         })
     }
 
     override fun onDestroy() {
-        shaded.dispose()
         super.onDestroy()
     }
 }
--- shaded/src/main/java/com/feresr/shaded/Shaded.kt
+++ shaded/src/main/java/com/feresr/shaded/Shaded.kt
@@ -6,57 +6,91 @@ import android.opengl.GLES20.GL_BLEND
 import android.opengl.GLES20.GL_DEPTH_TEST
 import android.opengl.GLES20.glDisable
 import android.util.Log
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors.newSingleThreadExecutor
 import kotlin.coroutines.CoroutineContext
 
-class Shaded(context: Context) : CoroutineScope {
+/* This class defines its own CoroutineContext with a SupervisorJob() and a single threaded
+ * dispatcher. This allows the class to be used independently of the caller scope.
+ * Disposing the job [dispose] will take care of cancelling any ongoing GL tasks
+ */
+// todo: Add exception handler here?
 
-    override val coroutineContext: CoroutineContext = SupervisorJob() +
-            newSingleThreadExecutor().asCoroutineDispatcher() + // OpenGL thread
-            CoroutineExceptionHandler { _, e -> Log.e(Shaded::class.java.simpleName, e.toString()) }
+suspend fun render(context: Context, block: suspend Renderer.() -> Unit) {
+    val shaded = Shaded(context)
+    shaded.init()
+    try {
+        shaded.block()
+    } finally {
+        shaded.dispose()
+    }
+}
+
+interface Renderer {
+    suspend fun upload(bitmap: Bitmap)
+    suspend fun render(target: Bitmap, filters: List<Filter>)
+}
+
+private class Shaded(private val context: Context) : Renderer {
 
     private lateinit var previewPingPongRenderer: PingPongRenderer
+    private val coroutineContext: CoroutineContext =
+        NonCancellable + newSingleThreadExecutor().asCoroutineDispatcher() + // OpenGL thread
+                CoroutineExceptionHandler { _, e ->
+                    Log.e(
+                        Shaded::class.java.simpleName,
+                        e.toString()
+                    )
+                }
 
-    init {
-        // Init OpenGL in its own thread
-        launch {
-            com.feresr.shaded.opengl.Context.init()
-            glDisable(GL_BLEND)
-            glDisable(GL_DEPTH_TEST)
-            Layer().bind()
-            previewPingPongRenderer = PingPongRenderer(Filter(context))
-        }
+    suspend fun init() = withContext(coroutineContext) {
+        com.feresr.shaded.opengl.Context.init()
+        glDisable(GL_BLEND)
+        glDisable(GL_DEPTH_TEST)
+        Layer().bind()
+        previewPingPongRenderer = PingPongRenderer(Filter(context))
     }
 
-    suspend fun upload(bitmap: Bitmap) = withContext(coroutineContext) {
+    override suspend fun upload(bitmap: Bitmap) = withContext(coroutineContext) {
         previewPingPongRenderer.setData(bitmap)
     }
 
-    suspend fun render(target: Bitmap, filters: List<Filter>) {
+    override suspend fun render(target: Bitmap, filters: List<Filter>): Unit =
         withContext(coroutineContext) {
             previewPingPongRenderer.render(target, filters)
         }
-    }
 
-    fun dispose() {
-        // not a suspending function: launch a coroutine independently of the parent scope (to avoid it being cancelled)
-        launch {
-            previewPingPongRenderer.delete()
-            com.feresr.shaded.opengl.Context.tearDown()
-            this@Shaded.cancel()
-        }
+    suspend fun dispose() = withContext(coroutineContext) {
+        previewPingPongRenderer.delete()
+        com.feresr.shaded.opengl.Context.tearDown()
     }
 
     companion object {
         init {
             System.loadLibrary("shaded")
         }
+    }
+}
+
+class GLDispatcher : CoroutineDispatcher() {
+    private val executor = newSingleThreadExecutor()
+
+    init {
+        executor.submit {
+            com.feresr.shaded.opengl.Context.init()
+        }
+    }
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        executor.submit(block)
+    }
+
+    companion object {
+        val instance: GLDispatcher by lazy { GLDispatcher() }
     }
 }
